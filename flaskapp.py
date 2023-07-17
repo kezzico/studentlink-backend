13de45900aeab7d9f9ecb7fbe55a0649c4b94130
Lee <lee@kezzi.co>
2023-07-17 16:21:29 -0700

    initial commit

---
+++ flaskapp.py
@@ -0,0 +1,13 @@
+# flaskapp.py
+
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello World"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port=5051)
+
