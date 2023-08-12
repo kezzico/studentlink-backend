f565e88011b66eb12f277a686160b17458456cef
Lee <lee@kezzi.co>
2023-08-11 18:20:50 -0700

    enable CORS when DEBUG=True

--- flaskapp.py
+++ flaskapp.py
@@ -1,7 +1,17 @@
 # flaskapp.py
 
 from flask import Flask, request, jsonify
+from flask_cors import CORS
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
+
 app = Flask(__name__)
+
+if config.get("DEBUG"):
+    CORS(app, origins=["http://localhost:3000"])
+    print("poo")
+
 
 @app.route("/")
 def index():
